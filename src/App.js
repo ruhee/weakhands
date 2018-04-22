68820705e9e2efe2a8dde101c16445b79fe8ad60
Ruhee Dewji <ruhee.dewji@gmail.com>
2018-04-22 14:38:19 -0400

    Update shows, add padding for mobile

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ class App extends Component {
           <h1 className="title">Weak Hands</h1>
         </header>
         <img src={band} alt="Weak Hands, the band" />
-        <p>
-          we play punk rock
-        </p> 
-        <p>
-          find us on <a href="http://facebook.com/weakhandsband">facebook</a>, <a href="http://twitter.com/weakhandsband">twitter</a> and <a href="http://instagram.com/weakhands">instagram</a>
-        </p>
-        <p>
-          next show: <a href="https://www.facebook.com/events/160232448018992/?ref=br_rs"><b>april 21st</b> at <b>handlebar</b></a> with mother tongues, eyeballs, and slutcode
-        </p>
+        <div className="text">
+          <p>
+            we play punk rock
+          </p> 
+          <p>
+            find us on <a href="http://facebook.com/weakhandsband">facebook</a>, <a href="http://twitter.com/weakhandsband">twitter</a> and <a href="http://instagram.com/weakhands">instagram</a>
+          </p>
+          <p>
+            next show: <a href="https://www.facebook.com/events/740651842807731/"><b>may 20th</b> at <b>the baby g</b></a> with shilpa ray &amp; bald britney
+          </p>
+        </div>
       </div>
     );
   }
